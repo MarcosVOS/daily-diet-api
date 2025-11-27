ad77931ac53f71210efe3dc56863b098a2ac21c1
MarcosVOS <marcos.oliveiradev@gmail.com>
2025-11-26 23:11:08 -0300

    build: runs the migrations when running the database configuration

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,7 @@
 import type { Knex } from "knex";
 import setupKnex from "knex";
 import { env } from "./env/index.ts";
+import { run } from "node:test";
 
 export const config: Knex.Config = {
   client: "pg",
@@ -16,5 +17,16 @@ export const config: Knex.Config = {
     directory: "./infra/db/migrations",
   },
 };
-
 export const knex = setupKnex(config);
+
+export async function runMigrations() {
+  try {
+    console.log("🔗 Conectando ao banco de dados e verificando migrações...");
+    const [batchNo, log] = await knex.migrate.latest();
+  } catch (error) {
+    console.error("❌ ERRO CRÍTICO: Falha ao executar as migrações.", error);
+    process.exit(1);
+  }
+}
+
+await runMigrations();
