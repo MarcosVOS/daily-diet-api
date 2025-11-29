419590a325f0413136c4b41d76bdd18c5e592d95
MarcosVOS <marcos.oliveiradev@gmail.com>
2025-11-29 00:26:18 -0300

    feat: implements crud for meals

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -1,10 +1,131 @@
 import type { FastifyInstance } from "fastify";
 import { checkSessionIdExists } from "../middleware/check-session-id-exists.ts";
+import knex from "knex";
+import { z } from "zod";
 
 export async function mealsRoutes(app: FastifyInstance) {
   app.addHook("preHandler", checkSessionIdExists);
-  app.get("/", async (request, reply) => {});
-  app.post("/", async (request, reply) => {});
-  app.put("/", async (request, reply) => {});
-  app.delete("/", async (request, reply) => {});
+
+  app.get("/", async (request, reply) => {
+    const user = request.user;
+    const meals = await knex("meals").where("user_id", user!.id).select("*");
+    return reply.status(200).send(meals);
+  });
+  app.post("/", async (request, reply) => {
+    const createMealsSchema = z.object({
+      name: z.string(),
+      description: z.string(),
+      is_on_diet: z.boolean(),
+    });
+
+    const result = createMealsSchema.safeParse(request.body);
+
+    if (!result.success) {
+      const missingFields = result.error.issues.map((issue) => issue.path[0]);
+      return reply.status(400).send({
+        error: "Bad Request",
+        message: `body must have required properties: ${missingFields.join(
+          ", ",
+        )}`,
+        statusCode: 400,
+      });
+    }
+
+    const createMeals = await knex("meals").insert({
+      name: result.data.name,
+      description: result.data.description,
+      is_on_diet: result.data.is_on_diet,
+      user_id: request.user!.id,
+    });
+
+    return reply.status(201).send();
+  });
+  app.put("/:id", async (request, reply) => {
+    const paramsSchema = z.object({
+      id: z.uuid(),
+    });
+
+    const resultParams = paramsSchema.safeParse(request.params);
+
+    if (!resultParams.success) {
+      return reply.status(400).send({
+        error: "Bad Request",
+        message: "params id must be a valid uuid",
+        statusCode: 400,
+      });
+    }
+
+    const updateMealsSchema = z.object({
+      name: z.string(),
+      description: z.string(),
+      is_on_diet: z.boolean(),
+    });
+
+    const resultBody = updateMealsSchema.safeParse(request.body);
+
+    if (!resultBody.success) {
+      const missingFields = resultBody.error.issues.map(
+        (issue) => issue.path[0],
+      );
+      return reply.status(400).send({
+        error: "Bad Request",
+        message: `body must have required properties: ${missingFields.join(
+          ", ",
+        )}`,
+        statusCode: 400,
+      });
+    }
+
+    const meal = await knex("meals")
+      .where({ id: resultParams.data.id, user_id: request.user!.id })
+      .first();
+
+    if (!meal) {
+      return reply.status(404).send({
+        error: "Not Found",
+        message: "meal not found",
+        statusCode: 404,
+      });
+    }
+
+    await knex("meals").where({ id: resultParams.data.id }).update({
+      name: resultBody.data.name,
+      description: resultBody.data.description,
+      is_on_diet: resultBody.data.is_on_diet,
+      updated_at: new Date(),
+    });
+
+    return reply.status(204).send();
+  });
+  app.delete("/:id", async (request, reply) => {
+    const paramsSchema = z.object({
+      id: z.uuid(),
+    });
+
+    const result = paramsSchema.safeParse(request.params);
+
+    if (!result.success) {
+      return reply.status(400).send({
+        error: "Bad Request",
+        message: "params id must be a valid uuid",
+        statusCode: 400,
+      });
+    }
+
+    const meal = await knex("meals")
+      .where({ id: result.data.id, user_id: request.user!.id })
+      .first();
+
+    if (!meal) {
+      return reply.status(404).send({
+        error: "Not Found",
+        message: "meal not found",
+        statusCode: 404,
+      });
+    }
+
+    await knex("meals").where({ id: result.data.id }).del();
+
+    return reply.status(204).send();
+  });
 }
