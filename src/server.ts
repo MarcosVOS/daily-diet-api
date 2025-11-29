8524aa286ddfefc047867ccb6bada55f9de6b986
MarcosVOS <marcos.oliveiradev@gmail.com>
2025-11-29 14:27:32 -0300

    refactor: move port to .env

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import { app } from "./app.ts";
+import { env } from "./env/index.ts";
 
 app
   .listen({
     port: 3000,
   })
   .then(() => {
-    console.log("HTTP server is running  ", 3000);
+    console.log("HTTP server is running  ", env.DAILY_DIET_API_PORT);
   });
